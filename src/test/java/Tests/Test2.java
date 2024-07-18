8cfbfaa796ed0db539410a6061fd23c8b63ac479
Loufreshistaken <enestasinmaa@gmail.com>
2024-07-18 10:17:26 +0300

    MultiTesting2

--- src/test/java/Tests/Test2.java
+++ src/test/java/Tests/Test2.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class Test2 {
 
     @Test (priority = 1)
-    public void testYoutube() {
+    public void testWikiPedia() {
         System.setProperty("wbbdriver.chrome.driver", "drivers/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
@@ -17,14 +17,14 @@ public class Test2 {
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.youtube.com/?app=desktop&hl=tr");
-        driver.findElement(By.xpath("/html/body/ytd-app/div[1]/tp-yt-app-drawer/div[2]/div/div[2]/div[2]/ytd-guide-renderer/div[1]/ytd-guide-section-renderer[1]/div/ytd-guide-entry-renderer[3]/a/tp-yt-paper-item/yt-formatted-string")).click();
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.cssSelector(".other-project:nth-child(1)")).click();
 
         driver.quit();
     }
 
     @Test (priority = 2)
-    public void testYoutube2() {
+    public void testWikiPedia2() {
         System.setProperty("wbbdriver.chrome.driver", "drivers/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
@@ -32,8 +32,8 @@ public class Test2 {
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.youtube.com/?app=desktop&hl=tr");
-        driver.findElement(By.xpath("/html/body/ytd-app/div[1]/tp-yt-app-drawer/div[2]/div/div[2]/div[2]/ytd-guide-renderer/div[1]/ytd-guide-section-renderer[1]/div/ytd-guide-entry-renderer[3]/a/tp-yt-paper-item/yt-formatted-string")).click();
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.cssSelector(".other-project:nth-child(2)")).click();
 
         driver.quit();
     }
